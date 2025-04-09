ca59041406471e19f0cbe848610df54fd37e8d27
Julius Cinco Cesar <knznsmn@knznsmn.com>
2025-04-09 23:48:28 +0400

    Add .vimrc

--- 42/c00/ex04/ft_print_negative.c
+++ 42/c00/ex04/ft_print_negative.c
@@ -1,22 +1,28 @@
+/* ****************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_negative.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knznsmn <mail@knznsmn>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 23:31:21 by knznsmn           #+#    #+#             */
+/*   Updated: 2025/04/09 23:31:21 by knznsmn          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar(char c) {
+void ft_putchar(char c)
+{
 	write(1, &c, 1);
 }
 
-void ft_is_negative(int n) {
-
+void ft_is_negative(int n)
+{
 	if (n % 2 == 0) {
 		ft_putchar('Y');
 	}
 	else {
 		ft_putchar('N');
 	}
-}
-
-
-int main()
-{
-	ft_is_negative(42);
-	ft_putchar('\n');
 }
