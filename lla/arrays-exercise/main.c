3c09ca712207b864ab7428901bc8b31e8cb5e44e
Julius Cinco Cesar <mail@knznsmn.com>
2025-09-27 07:33:07 +0400

    Add arrays exercise solution

---
+++ lla/arrays-exercise/main.c
@@ -0,0 +1,40 @@
+/*****************************************************************************/
+/*   arrays-exercise/main.c                                                  */
+/*                                                                           */
+/*   By:      knznsmn                                                        */
+/*   Email:   mail@knznsmn.com                                               */
+/*   Created: 2025/09/26 22:24:53                                            */
+/*   Updated: 2025/09/26 22:24:53                                            */
+/*                                                                           */
+/*   Codes for days.                                                         */
+/*****************************************************************************/
+
+#include <stdio.h>
+#define MAX 10
+
+int main(int argc, char **argv) {
+
+  int myarrays[MAX] = {0};
+  for (int i = 0; i < MAX; i++) {
+    printf("%d", myarrays[i]);
+    if (i == MAX - 1) {
+      printf(".\n");
+    }
+    else {
+      printf(", ");
+    }
+  }
+  printf("Setting 4th element to 2...\n\n");
+  printf("Printing the new array...\n");
+  myarrays[3] = 2;
+  for (int i = 0; i < MAX; i++) {
+    printf("%d", myarrays[i]);
+    if (i == MAX - 1) {
+      printf(".\n");
+    }
+    else {
+      printf(", ");
+    }
+  }
+  return 0;
+}
