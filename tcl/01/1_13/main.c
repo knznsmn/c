4d348f186dcf6ea27ad55de51a8edc9ed5c56972
Julius Cinco Cesar <mail@jccesar.com>
2026-01-02 15:54:49 +0400

    Added time.h exercises

---
+++ tcl/01/1_13/main.c
@@ -0,0 +1,24 @@
+/*****************************************************************************/
+/*   1_13/main.c                                                             */
+/*                                                                           */
+/*   By:      knznsmn                                                        */
+/*   Email:   mail@jccesar.com                                               */
+/*   Created: 2026/01/02 13:45:14                                            */
+/*   Updated: 2026/01/02 13:45:14                                            */
+/*                                                                           */
+/*   Codes for days.                                                         */
+/*****************************************************************************/
+
+/*
+  Exercise 1-13. Write a program to print a histogram of the lengths of
+  words in its input. It is easy to draw the histogram with the bars
+  horizontal; a vertical orientation is more challenging. 
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int c;
+
+}
