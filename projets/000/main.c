faddd7dfe5edcf21045722132d1455fa141a6475
Julius Cinco Cesar <mail@jccesar.com>
2026-01-09 11:55:53 +0400

    Updated project 000

--- projets/000/main.c
+++ projets/000/main.c
@@ -11,19 +11,13 @@
 
 #include "headers.c"
 
-typedef struct {
-  char *name;
-  char *info;
-} Detail;
-
-enum {
-  HEADER_WIDTH = 80
-};
-
-void padding(int *f, const int n);
 void newline(int *f);
+void padding(int *f, const int n);
 void row_line(const Header *hr, int *f);
-void row_mois(const Header *hr, int *f, Detail *t);
+void row_void(const Header *hr, int *f);
+void row_info(const Header *hr, char *s, int *f);
+void row_mois(const Header *hr, Detail *t, int *f);
+void row_file(const Header *hr, const Filename *fl, int *f);
 
 int main(int argc, char **argv) {
 
@@ -43,20 +37,41 @@ int main(int argc, char **argv) {
       return 1;
     }
     
-    Header *header = create_hr(argv[1]);
+
+    Filename file;
+    file.name = extract_fil(argv[1]);
+    file.exts = extract_ext(argv[1]);
+    file.name_size = ft_strlen(file.name);
+    file.exts_size = ft_strlen(file.exts);
+    
+    Header *header = create_hr(file.exts);
+    
     Detail name;
-    Detail mail;
     name.name = "By:";
     name.info = "knznsmn";
+    name.name_size = ft_strlen(name.name);
+    name.info_size = ft_strlen(name.info);
+
+    Detail mail;
     mail.name = "Email:";
     mail.info = "mail@jccesar.com";
+    mail.name_size = ft_strlen(mail.name);
+    mail.info_size = ft_strlen(mail.info);
 
-void newline(int *f);
-    row_line(header, &fd);
-    row_mois(header, &fd, &name);
-    row_mois(header, &fd, &mail);
-    row_line(header, &fd);
+    char *info = "Codes for days.";
 
+    row_line(header, &fd);
+    row_file(header, &file, &fd);
+    row_void(header, &fd);
+    row_mois(header, &name, &fd);
+    row_mois(header, &mail, &fd);
+    row_void(header, &fd);
+    row_info(header, info, &fd);
+    row_line(header, &fd);
+    newline(&fd);
+    
+    free(file.name);
+    free(file.exts);
     destroy_hr(&header);
     close(fd);
   }
@@ -78,46 +93,82 @@ void newline(int *f) {
 void row_line(const Header *hr, int *f) {
   if (hr == NULL || f == NULL) return;
 
-  int number_of_hr_lines = 80;
-  if (ft_strcmp(hr->hr_extf, ".lua")) {
-    number_of_hr_lines = 78;
+  int borders_size = hr->hr_open_size + hr->hr_done_size;
+  int number_of_hr_lines = HEADER_WIDTH - borders_size;
+
+  write(*f, hr->hr_open, hr->hr_open_size);
+  for (int i = 0; i < number_of_hr_lines; i++) {
+    write(*f, hr->hr_line, hr->hr_line_size);
   }
-  else if (ft_strcmp(hr->hr_extf, ".html")) {
-    number_of_hr_lines = 77;
-  }
+  write(*f, hr->hr_done, hr->hr_done_size);
+  newline(f);
+}
+void row_file(const Header *hr, const Filename *fl, int *f) {
+  if (hr == NULL || fl == NULL) return;
+
+  char slash = '/';
+  size_t border_left_size = ft_strlen(hr->hr_open);
+  size_t border_righ_size = ft_strlen(hr->hr_done);
+  size_t border_size = border_left_size + border_righ_size;
+  size_t total_space = HEADER_WIDTH - border_size;
+  size_t left_column = LEFT_PADDING + fl->name_size + 1 + fl->exts_size;
+  size_t r_col_copad = total_space - left_column;
 
   write(*f, hr->hr_open, ft_strlen(hr->hr_open));
-  for (int i = 0; i < number_of_hr_lines; i++) {
-    write(*f, hr->hr_line, ft_strlen(hr->hr_line));
-  }
+  padding(f, LEFT_PADDING);
+  write(*f, fl->name, fl->name_size);
+  write(*f, &slash, 1);
+  write(*f, fl->exts, fl->exts_size);
+  padding(f, r_col_copad);
   write(*f, hr->hr_done, ft_strlen(hr->hr_done));
   newline(f);
 }
-void row_date(const Header *hr, const char *s) {
-  const char *ext = extract_ext(s);
 
-}
-
-void row_mois(const Header *hr, int *f, Detail *t) {
+void row_mois(const Header *hr, Detail *t, int *f) {
   if (hr == NULL || f == NULL) return;
 
-  size_t border_left_size = ft_strlen(hr->hr_open);
-  size_t border_righ_size = ft_strlen(hr->hr_done);
-  size_t borders_size = border_left_size + border_righ_size;
+  size_t borders_size = hr->hr_open_size + hr->hr_done_size;
   size_t total_spaces = HEADER_WIDTH - borders_size;
 
-  size_t left_padding = 3;
-  size_t left_columns = 9;
-  size_t left_col_pad = left_columns - ft_strlen(t->name);
+  size_t left_content = 9;
+  size_t left_columns = left_content + LEFT_PADDING;
+  size_t left_col_pad = left_content - t->name_size;
   size_t right_column = total_spaces - left_columns;
-  size_t right_col_pd = right_column - ft_strlen(t->info);
+  size_t right_col_pd = right_column - t->info_size;
 
-  write(*f, hr->hr_open, ft_strlen(hr->hr_open));
-  padding(f, left_padding);
-  write(*f, t->name, ft_strlen(t->name));
+  write(*f, hr->hr_open, hr->hr_open_size);
+  padding(f, LEFT_PADDING);
+  write(*f, t->name, t->name_size);
   padding(f, left_col_pad);
-  write(*f, t->info, ft_strlen(t->info));
+  write(*f, t->info, t->info_size);
   padding(f, right_col_pd);
-  write(*f, hr->hr_done, ft_strlen(hr->hr_done));
+  write(*f, hr->hr_done, hr->hr_done_size);
+  newline(f);
+}
+
+void row_void(const Header *hr, int *f) {
+
+  size_t borders_size = hr->hr_open_size + hr->hr_done_size;
+  size_t total_spaces = HEADER_WIDTH - borders_size;
+  
+  write(*f, hr->hr_open, hr->hr_open_size);
+  padding(f, total_spaces);
+  write(*f, hr->hr_done, hr->hr_done_size);
+  newline(f);
+}
+
+void row_info(const Header *hr, char *s, int *f) {
+
+  size_t s_size = ft_strlen(s);
+  size_t borders_size = hr->hr_open_size + hr->hr_done_size;
+  size_t total_spaces = HEADER_WIDTH - borders_size;
+  size_t left_content = LEFT_PADDING + s_size;
+  size_t right_col_pd = total_spaces - left_content;
+
+  write(*f, hr->hr_open, hr->hr_open_size);
+  padding(f, LEFT_PADDING);
+  write(*f, s, s_size);
+  padding(f, right_col_pd);
+  write(*f, hr->hr_done, hr->hr_done_size);
   newline(f);
 }
--- projets/000/utilities.c
+++ projets/000/utilities.c
@@ -1,15 +1,30 @@
-
-enum {
-  hr_size = 2
-};
-
 typedef struct {
-  char *hr_extf;
   char *hr_open;
   char *hr_done;
   char *hr_line;
+  size_t hr_open_size;
+  size_t hr_done_size;
+  size_t hr_line_size;
 } Header;
 
+typedef struct {
+  char *name;
+  char *info;
+  size_t name_size;
+  size_t info_size;
+} Detail;
+
+typedef struct {
+  char *name;
+  char *exts;
+  size_t name_size;
+  size_t exts_size;
+} Filename;
+
+enum {
+  HEADER_WIDTH = 80,
+  LEFT_PADDING = 3,
+};
 
 size_t ft_strlen(const char *s) {
   if (!s) {
@@ -73,21 +88,32 @@ char *extract_ext(char *s) {
   return ext;
 }
 
+char *extract_fil(char *s) {
+  if (!s) return NULL;
+
+  size_t len = ft_strlen(s);
+  size_t i = len;
+  while (s[i] != '.') {
+    i--;
+  }
+  char *filename = malloc(i + 1);
+  len = 0;
+  while (len < i) {
+    filename[len] = s[len];
+    len++;
+  }
+  filename[len] = '\0';
+
+  return filename;
+}
+
 Header *create_hr(char *s) {
   if (s == NULL) return NULL;
-
-  char *ext = extract_ext(s);
-  if (ext == NULL) return NULL;
 
   const size_t BYTES = 4;
   Header *hr = malloc(sizeof *hr);
   if (hr == NULL) return NULL;
 
-  hr->hr_extf = malloc(ft_strlen(ext) + 1);
-  if (hr->hr_extf == NULL) {
-    free(hr);
-    return NULL;
-  }
   hr->hr_open = malloc(BYTES + 1);
   if (hr->hr_open == NULL) {
     free(hr);
@@ -105,24 +131,22 @@ Header *create_hr(char *s) {
     return NULL;
   }
 
-  ft_strcpy(hr->hr_extf, ext); 
-
-  if (ft_strcmp(".lua", ext)) {
+  if (ft_strcmp(".lua", s)) {
     ft_strcpy(hr->hr_open, "--[[");
     ft_strcpy(hr->hr_done, "]]");
     ft_strcpy(hr->hr_line, " ");
   }
-  else if (ft_strcmp(".html", ext)) {
+  else if (ft_strcmp(".html", s)) {
     ft_strcpy(hr->hr_open, "<!--");
     ft_strcpy(hr->hr_done, "-->");
     ft_strcpy(hr->hr_line, "-");
   }
-  else if (ft_strcmp(".gd", ext) || (ft_strcmp(".py", ext))) {
+  else if (ft_strcmp(".gd", s) || (ft_strcmp(".py", s))) {
     ft_strcpy(hr->hr_open, "\"\"\"");
     ft_strcpy(hr->hr_done, "\"\"\"");
     ft_strcpy(hr->hr_line, " ");
   }
-  else if (ft_strcmp(".sh", ext)) {
+  else if (ft_strcmp(".sh", s)) {
     ft_strcpy(hr->hr_open, ": '");
     ft_strcpy(hr->hr_done, "'");
     ft_strcpy(hr->hr_line, " ");
@@ -133,11 +157,14 @@ Header *create_hr(char *s) {
     ft_strcpy(hr->hr_line, "*");
   }
 
+  hr->hr_open_size = ft_strlen(hr->hr_open);
+  hr->hr_done_size = ft_strlen(hr->hr_done);
+  hr->hr_line_size = ft_strlen(hr->hr_line);
+
   return hr;
 }
 
 void destroy_hr(Header **hr) {
-  free((*hr)->hr_extf);
   free((*hr)->hr_open);
   free((*hr)->hr_done);
   free((*hr)->hr_line);
